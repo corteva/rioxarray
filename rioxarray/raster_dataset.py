3c14c87db08993302e4785df6a09ea77202ccc2f
David Hoese <david.hoese@ssec.wisc.edu>
2021-02-01 09:15:57 -0600

    Cleanup to_raster return documentation
    Co-authored-by: Alan D. Snow <alansnow21@gmail.com>

--- rioxarray/raster_dataset.py
+++ rioxarray/raster_dataset.py
@@ -375,7 +375,7 @@ class RasterDataset(XRasterBase):
         Returns
         -------
         :obj:`dask.Delayed`:
-            If the data array is a dask array, windowed is True, and compute
+            If the data array is a dask array and compute
             is True. Otherwise None is returned.
 
         """
