d39034ff7602a2c6d99bd63d3fdf53db257d95d3
Alessandro Amici <a.amici@bopen.eu>
2021-04-05 18:21:34 +0200

    Enable all `open_rasterio` in `open_dataset` as `backend_kwargs` and `**kwargs`

--- rioxarray/xarray_plugin.py
+++ rioxarray/xarray_plugin.py
@@ -8,10 +8,30 @@ CAN_OPEN_EXT = {"geotif", "geotiff", "j2k", "jp2", "tif", "tiff", "vrt"}
 
 
 class GdalBackend(xr.backends.common.BackendEntrypoint):
-    def open_dataset(self, filename_or_obj, drop_variables=None):
-        ds = _io.open_rasterio(filename_or_obj)
+    def open_dataset(
+        self,
+        filename_or_obj,
+        drop_variables=None,
+        mask_and_scale=False,
+        parse_coordinates=None,
+        lock=None,
+        masked=False,
+        variable=None,
+        group=None,
+        default_name="band_data",
+    ):
+        ds = _io.open_rasterio(
+            filename_or_obj,
+            mask_and_scale=mask_and_scale,
+            parse_coordinates=parse_coordinates,
+            lock=lock,
+            masked=masked,
+            variable=variable,
+            group=group,
+            default_name=default_name,
+        )
         if isinstance(ds, xr.DataArray):
-            ds = ds.to_dataset(name="band_data")
+            ds = ds.to_dataset()
         ds = ds.reset_coords("spatial_ref")
         return ds
 
