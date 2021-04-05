1d347804a4ed26cd8a598c61eedf11be08eb0ece
Alessandro Amici <a.amici@bopen.eu>
2021-04-05 12:58:26 +0200

    Add a test intended to work with xarray `master`

---
+++ test/integration/test_integration_xarray_plugin.py
@@ -0,0 +1,14 @@
+import os.path
+import xarray as xr
+
+from test.conftest import TEST_INPUT_DATA_DIR
+
+
+def test_xarray_open_dataset():
+    cog_file = os.path.join(TEST_INPUT_DATA_DIR, "cog.tif")
+
+    ds = xr.open_dataset(cog_file)
+
+    assert isinstance(ds, xr.Dataset)
+    assert "band1" in ds.data_vars
+    assert ds.data_vars["band1"].shape == (500, 500)
