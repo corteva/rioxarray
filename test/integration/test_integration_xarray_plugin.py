6928b5e7318abd773d981c81e5f87f64784b2b97
Alessandro Amici <a.amici@bopen.eu>
2021-04-05 13:36:50 +0200

    Test with and without guessing the extension

--- test/integration/test_integration_xarray_plugin.py
+++ test/integration/test_integration_xarray_plugin.py
@@ -10,8 +10,12 @@ xr = pytest.importorskip("xarray", minversion="0.17.1.dev0")
 def test_xarray_open_dataset():
     cog_file = os.path.join(TEST_INPUT_DATA_DIR, "cog.tif")
 
-    ds = xr.open_dataset(cog_file)
+    ds = xr.open_dataset(cog_file, engine="gdal")
 
     assert isinstance(ds, xr.Dataset)
     assert "band1" in ds.data_vars
     assert ds.data_vars["band1"].shape == (500, 500)
+
+    ds = xr.open_dataset(cog_file)
+
+    assert isinstance(ds, xr.Dataset)
