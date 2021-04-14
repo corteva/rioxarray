ad7ea9749bffb051a79efb289c54f639bd9585e6
snowman2 <alansnow21@gmail.com>
2021-04-13 20:11:32 -0500

    ENH: Add support for 'drop_variables' in rasterio engine

--- test/integration/test_integration_xarray_plugin.py
+++ test/integration/test_integration_xarray_plugin.py
@@ -27,6 +27,34 @@ def test_xarray_open_dataset():
     assert isinstance(ds, xr.Dataset)
 
 
+def test_xarray_open_dataset__drop_variables():
+    input_file = os.path.join(
+        TEST_INPUT_DATA_DIR, "MOD09GA.A2008296.h14v17.006.2015181011753.hdf"
+    )
+
+    rds = xr.open_dataset(
+        input_file,
+        engine="rasterio",
+        group="MODIS_Grid_500m_2D",
+        drop_variables=[
+            "sur_refl_b01_1",
+            "sur_refl_b02_1",
+            "sur_refl_b03_1",
+            "sur_refl_b04_1",
+            "sur_refl_b05_1",
+            "sur_refl_b06_1",
+            "sur_refl_b07_1",
+        ],
+    )
+
+    assert sorted(rds.data_vars) == [
+        "QC_500m_1",
+        "iobs_res_1",
+        "num_observations_500m",
+        "obscov_500m_1",
+    ]
+
+
 def test_open_multiple_resolution():
     with pytest.raises(
         RioXarrayError,
