9a05c913be59cc74a0e594a04b2c820d3bfe6e49
Andrew Annex <andrew.annex@gmail.com>
2021-07-29 20:02:55 -0400

    added test for isel_window with padding, unhandeled situation when making same requests without padding

--- rioxarray/rioxarray.py
+++ rioxarray/rioxarray.py
@@ -917,12 +917,19 @@ class XRasterBase:
         row_stop = math.floor(row_stop) if row_stop < 0 else math.ceil(row_stop)
         col_start = math.ceil(col_start) if col_start < 0 else math.floor(col_start)
         col_stop = math.floor(col_stop) if col_stop < 0 else math.ceil(col_stop)
+        if row_start < 0:
+            row_stop -= row_start
+            row_start = 0
+        if col_start < 0:
+            col_stop -= col_start
+            col_start = 0
         row_slice = slice(int(row_start), int(row_stop))
         col_slice = slice(int(col_start), int(col_stop))
         array_subset = self._obj
         if pad:
-            array_subset = array_subset.pad_box(*rasterio.windows.bounds(window, self.transform(recalc=True)))
+            array_subset = array_subset.rio.pad_box(*rasterio.windows.bounds(window, self.transform(recalc=True)))
         return (
+            # I can't have any negative indexes
             array_subset.isel({self.y_dim: row_slice, self.x_dim: col_slice})
             .copy()  # this is to prevent sharing coordinates with the original dataset
             .rio.set_spatial_dims(x_dim=self.x_dim, y_dim=self.y_dim, inplace=True)
