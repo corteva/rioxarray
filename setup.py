8874c2303032b0995eb068d82998d6f5f27a266f
Alan D. Snow <alansnow21@gmail.com>
2022-04-04 16:24:34 -0500

    REF: use setup.cfg (#500)

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@
 
 import os
 import sys
-from itertools import chain
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_version():
@@ -20,56 +19,4 @@ def get_version():
     sys.exit("ERROR: rioxarray version not fount.")
 
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-requirements = ["packaging", "rasterio>=1.1.1", "xarray>=0.17", "pyproj>=2.2"]
-
-test_requirements = ["pytest>=3.6", "pytest-cov", "pytest-timeout", "dask", "netcdf4"]
-doc_requirements = ["sphinx-click", "nbsphinx", "sphinx_rtd_theme"]
-
-extras_require = {
-    "interp": ["scipy"],
-    "doc": doc_requirements,
-    "dev": test_requirements
-    + doc_requirements
-    + [
-        "pylint",
-        "pre-commit",
-    ],
-}
-extras_require["all"] = list(chain.from_iterable(extras_require.values()))
-
-setup(
-    author="rioxarray Contributors",
-    author_email="alansnow21@gmail.com",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Topic :: Scientific/Engineering :: GIS",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    description="rasterio xarray extension.",
-    install_requires=requirements,
-    license="BSD license",
-    long_description=readme + "\n\n",
-    include_package_data=True,
-    keywords="rioxarray,xarray,rasterio",
-    name="rioxarray",
-    packages=find_packages(include=["rioxarray*"]),
-    test_suite="test",
-    tests_require=test_requirements,
-    extras_require=extras_require,
-    url="https://github.com/corteva/rioxarray",
-    version=get_version(),
-    zip_safe=False,
-    python_requires=">=3.8",
-    entry_points={
-        "xarray.backends": ["rasterio=rioxarray.xarray_plugin:RasterioBackend"]
-    },
-)
+setup(version=get_version())
