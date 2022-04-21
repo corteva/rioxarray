a77db88ca27912dbdcba6930cd9af03a1b2ea79c
snowman2 <alansnow21@gmail.com>
2022-04-21 11:06:21 -0500

    TST: Allow pre-release versions in 1.3 rasterio comparison

--- test/conftest.py
+++ test/conftest.py
@@ -16,7 +16,9 @@ PYPROJ_LT_3 = version.parse(pyproj.__version__) < version.parse("3")
 RASTERIO_LT_122 = version.parse(rasterio.__version__) < version.parse("1.2.2")
 RASTERIO_EQ_122 = version.parse(rasterio.__version__) == version.parse("1.2.2")
 RASTERIO_GE_125 = version.parse(rasterio.__version__) >= version.parse("1.2.5")
-RASTERIO_GE_13 = version.parse(rasterio.__version__) >= version.parse("1.3.0")
+RASTERIO_GE_13 = version.parse(
+    version.parse(rasterio.__version__).base_version
+) >= version.parse("1.3.0")
 
 
 # xarray.testing.assert_equal(input_xarray, compare_xarray)
