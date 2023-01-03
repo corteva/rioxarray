b323754273affce6806abdc096d964ae61a9af64
Alan D. Snow <alansnow21@gmail.com>
2023-01-03 09:39:19 -0600

    MNT: Update copyright year to 2023 (#623)

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "rioxarray"
-copyright = "2019, rioxarray Contributors"
+copyright = "2019-2023, Corteva Agriscience™"
 author = "rioxarray Contributors"
 
 # The version info for the project you're documenting, acts as replacement
