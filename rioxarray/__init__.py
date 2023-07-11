66dc7ca71b276be44f94104d3a0d3ed0b4391c95
Kirill Kouzoubov <kirill888@gmail.com>
2023-07-12 09:29:14 +1000

    MNT: add __all__ to top level module #680

--- rioxarray/__init__.py
+++ rioxarray/__init__.py
@@ -4,8 +4,16 @@ import importlib.metadata
 
 import rioxarray.raster_array  # noqa
 import rioxarray.raster_dataset  # noqa
-from rioxarray._io import open_rasterio  # noqa
-from rioxarray._options import set_options  # noqa
-from rioxarray._show_versions import show_versions  # noqa
+from rioxarray._io import open_rasterio
+from rioxarray._options import set_options
+from rioxarray._show_versions import show_versions
 
 __version__ = importlib.metadata.version(__package__)
+
+__all__ = [
+    "open_rasterio",
+    "set_options",
+    "show_versions",
+    "__author__",
+    "__version__",
+]
